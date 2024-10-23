900da87f2f1a9136a134fe1c06e3de982ebb5578
Decencies <decenciesmc@gmail.com>
2024-10-23 19:51:43 +0100

    error: add error message when attempting to reassign incompatible oop type

--- src/main/kotlin/land/src/toolbox/jvm/cache/Oops.kt
+++ src/main/kotlin/land/src/toolbox/jvm/cache/Oops.kt
@@ -3,6 +3,7 @@ package land.src.toolbox.jvm.cache
 import land.src.toolbox.jvm.primitive.Oop
 import land.src.toolbox.jvm.Scope
 import kotlin.reflect.KClass
+import kotlin.reflect.full.isSubclassOf
 
 class Oops(scope: Scope) : Scope by scope {
     private val cache = mutableMapOf<Long, Oop?>()
@@ -13,6 +14,9 @@ class Oops(scope: Scope) : Scope by scope {
     operator fun invoke(address: Long, oopType: KClass<*>): Oop? {
         val currentType = cache[address]?.let { it::class } ?: oopType
         if (currentType != oopType) {
+            require(currentType.java.isAssignableFrom(oopType.java)) {
+                "Replacement oop type must be assignable from the current type"
+            }
             val oop = structs(address, oopType) as? Oop?
             cache[address] = oop
             return oop
