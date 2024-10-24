a63493e86b992269f1f5f8f1a9403d81b8440b26
Decencies <decenciesmc@gmail.com>
2024-10-24 22:58:41 +0100

    fix: throw error

--- src/main/kotlin/land/src/toolbox/jvm/primitive/Struct.kt
+++ src/main/kotlin/land/src/toolbox/jvm/primitive/Struct.kt
@@ -7,7 +7,7 @@ import land.src.toolbox.jvm.Scope
  */
 abstract class Struct(val address: Address) : Scope by address {
     val type: Type by lazy {
-        vm.type(typeName ?: this::class.simpleName ?: "No type name")
+        vm.type(typeName ?: this::class.simpleName ?: error("No type name provided"))
     }
 
     open val typeName: String? = null
