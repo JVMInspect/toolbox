b4cc06f7712bf2248997f642b4b109634c637a12
Decencies <decenciesmc@gmail.com>
2024-10-24 01:11:43 +0100

    fix: inner class writing (2x)

--- src/main/kotlin/land/src/toolbox/jvm/oop/InnerClassesIterator.kt
+++ src/main/kotlin/land/src/toolbox/jvm/oop/InnerClassesIterator.kt
@@ -13,24 +13,32 @@ class InnerClassInfo(
 class InnerClassesIterator(scope: Scope, ik: InstanceKlass) : Scope by scope, Iterator<InnerClassInfo> {
     private var index = 0
     private val innerClasses = ik.innerClasses
-    val length = innerClasses?.length ?: 0
-    val entries = length / 4
+    var size = innerClasses?.length ?: 0
+    val entries get() = size / innerClassNextOffset
 
     private val innerClassAccessFlagsOffset: Int by constant("InstanceKlass::inner_class_access_flags_offset")
     private val innerClassInnerClassInfoOffset: Int by constant("InstanceKlass::inner_class_inner_class_info_offset")
     private val innerClassInnerNameOffset: Int by constant("InstanceKlass::inner_class_inner_name_offset")
     private val innerClassNextOffset: Int by constant("InstanceKlass::inner_class_next_offset")
     private val innerClassOuterClassInfoOffset: Int by constant("InstanceKlass::inner_class_outer_class_info_offset")
+    private val innerClassEnclosingMethodAttributeSize: Int by constant("InstanceKlass::enclosing_method_attribute_size")
 
-    override fun hasNext() = length > index
+    init {
+        if (size % innerClassNextOffset == innerClassEnclosingMethodAttributeSize)
+            size -= innerClassEnclosingMethodAttributeSize
+    }
+
+    override fun hasNext() = index < size
 
     override fun next(): InnerClassInfo {
-        index += innerClassNextOffset
         val array = innerClasses!!
         val access = array[index + innerClassAccessFlagsOffset]!!
         val classInfo = array[index + innerClassInnerClassInfoOffset]!!
         val className = array[index + innerClassInnerNameOffset]!!
         val outerClassInfo = array[index + innerClassOuterClassInfoOffset]!!
+
+        index += innerClassNextOffset
+
         return InnerClassInfo(access, classInfo, className, outerClassInfo)
     }
 }
