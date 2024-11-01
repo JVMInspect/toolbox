46a8cbfbb200364c2c968ec6136b97c03c3f3c66
Decencies <decenciesmc@gmail.com>
2024-11-01 23:09:13 +0000

    fix: use address instead of offset
    Signed-off-by: Decencies <decenciesmc@gmail.com>

--- src/main/kotlin/land/src/toolbox/jvm/oop/VMSymbols.kt
+++ src/main/kotlin/land/src/toolbox/jvm/oop/VMSymbols.kt
@@ -1,11 +1,11 @@
 package land.src.toolbox.jvm.oop
 
-import land.src.toolbox.jvm.dsl.offset
+import land.src.toolbox.jvm.dsl.address
 import land.src.toolbox.jvm.primitive.Address
 import land.src.toolbox.jvm.primitive.Struct
 
 class VMSymbols(address: Address): Struct(address) {
-    private val symbols: Long by offset("_vm_symbols[0]")
+    private val symbols: Long by address("_vm_symbols[0]")
 
     fun lookupSymbol(index: Int): Symbol {
         val symbolAddress = unsafe.getAddress(symbols + index * type.size)
