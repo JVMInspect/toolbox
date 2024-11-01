2983818b73fefe78039b1f1f257bf3826c76e668
Decencies <decenciesmc@gmail.com>
2024-11-01 23:50:00 +0000

    fix: casting
    Signed-off-by: Decencies <decenciesmc@gmail.com>

--- src/main/kotlin/land/src/toolbox/jvm/oop/nmethod.kt
+++ src/main/kotlin/land/src/toolbox/jvm/oop/nmethod.kt
@@ -4,12 +4,10 @@ import land.src.toolbox.jvm.dsl.maybeNull
 import land.src.toolbox.jvm.dsl.nonNull
 import land.src.toolbox.jvm.primitive.Address
 
-class nmethod(address: Address) : CompiledMethod(address) {
-
+class NMethod(address: Address) : CompiledMethod(address) {
     val entryPoint: Long by nonNull("_entry_point")
     val verifiedEntryPoint: Long by nonNull("_verified_entry_point")
-    val osrLink: nmethod? by maybeNull("_osr_link")
+    val osrLink: NMethod? by maybeNull("_osr_link")
 
     override val typeName = "nmethod"
-
 }
