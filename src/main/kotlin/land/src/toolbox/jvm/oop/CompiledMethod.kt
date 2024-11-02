5da9e9f0b9e36a045fd1aa09dabdd62d26bd07f7
Decencies <decenciesmc@gmail.com>
2024-11-02 00:06:11 +0000

    misc: create class for enum constants
    Signed-off-by: Decencies <decenciesmc@gmail.com>

--- src/main/kotlin/land/src/toolbox/jvm/oop/CompiledMethod.kt
+++ src/main/kotlin/land/src/toolbox/jvm/oop/CompiledMethod.kt
@@ -5,21 +5,13 @@ import land.src.toolbox.jvm.dsl.nonNull
 import land.src.toolbox.jvm.primitive.Address
 import land.src.toolbox.jvm.primitive.Struct
 
-val not_marked = 0
-val deoptimize = 1
-val deoptimize_noupdate = 2
+object MarkForDeoptimizationStatus {
+    const val not_marked = 0
+    const val deoptimize = 1
+    const val deoptimize_noupdate = 2
+}
 
 open class CompiledMethod(address: Address) : Struct(address) {
-
-    /**
-     *   enum MarkForDeoptimizationStatus {
-     *     not_marked,
-     *     deoptimize,
-     *     deoptimize_noupdate
-     *   };
-     *
-     */
-
     // _mark_for_deoptimization_status
     val markForDeoptimizationStatus: Int by nonNull {
         offset(0)
